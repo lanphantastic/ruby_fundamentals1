d5c953b196acb6631832eb105e8c1caf094fbba7
Lan Phan <lanc_phan@hotmail.com>
2017-09-18 12:57:50 -0400

    question 2 and 3

--- exercise2.rb
+++ exercise2.rb
@@ -3,3 +3,14 @@
 price = 55.0
 tip = 1.15
 puts "Typically, tip is usually 15% so the total price for a 55 dollar meal is #{price*tip}"
+
+# Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+
+integer = 10
+puts "Ten in its integer form is #{integer}"
+
+
+# Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+number_01 = 45628
+number_02 = 7839
+puts "The answer to #{number_01} when multiplied with #{number_02} is #{number_01*number_02}"
