a830299321128d15b3d0f265859f3fa1dcd35361
Lan Phan <lanc_phan@hotmail.com>
2017-09-18 12:46:33 -0400

    first question answered

--- exercise2.rb
+++ exercise2.rb
@@ -1,1 +1,5 @@
 # How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+
+price = 55.0
+tip = 1.15
+puts "Typically, tip is usually 15% so the total price for a 55 dollar meal is #{price*tip}"
