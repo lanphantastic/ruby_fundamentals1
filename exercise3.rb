487bd57f428f4b8d4d3a68615ffd02b08b8ffedf
Lan Phan <lanc_phan@hotmail.com>
2017-09-18 13:39:38 -0400

    With gets, you'll have the \n whereas the gets.chomp will be simply the number without starting a new line.

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,11 @@
 puts "What is your name?"
 user_name = gets
 puts "Hello, #{user_name}"
+
+# You may have also seen gets.chomp in other Ruby tutorials. chomp removes unwanted whitespace from the end of your user's input. In irb, try using both gets on its own as well as gets.chomp and look for the difference between the values that they return.
+
+puts "How old are you?"
+age1 = gets
+age2 = gets.chomp
+puts age1
+puts age2
