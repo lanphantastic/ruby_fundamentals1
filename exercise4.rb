64d90cb11e78600cbe4bee039992cc9e4b9b88e8
Lan Phan <lanc_phan@hotmail.com>
2017-09-18 13:46:30 -0400

    if statement for large number

---
+++ exercise4.rb
@@ -0,0 +1,11 @@
+# Ask the user to enter a number. Use an if statement to print "that's a big number!" if the number is 100 or more, or "why dream a little bigger?" otherwise.
+
+puts "Enter a number"
+
+number = gets.to_i
+
+if number > 50
+  puts "that's a big number"
+elsif number >= 100
+  puts "why dream a little bigger?"
+end
